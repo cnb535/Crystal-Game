a69edf10b07253f11a6eb19f2659bc1ca4febe72
Christina Bertram <Jamerican92grl@MacBook-Pro.local>
2018-04-24 18:47:08 -0400

    The game.js file has been updated to change the wins/losses count when the user matches or go over.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,13 @@
 // $(document).ready(function() {
 
 	let totalScore = 0;
+	$("#number").html(totalScore);
+
 	let wins = 0;
+	$("#wins").html(wins);
 	let losses = 0;
+	$("#losses").html(losses);
+
 
 	const getRandomIntInclusive = function(min, max) {
 		min = Math.ceil(min);
@@ -10,24 +15,100 @@
 		return Math.floor(Math.random() * (max - min + 1)) + min;
 	}
 
-
 	const randomNumber = getRandomIntInclusive(19, 120);
 	console.log(randomNumber);
 
 	$("#random-number").html(randomNumber);
+
 
 	const randomCrystal = getRandomIntInclusive(1,12);
 
 	$("#crystal-1").on("click", function() {
 		console.log(randomCrystal);
 
+		totalScore += randomCrystal;
 
+		$("#number").html(totalScore);
+
+		if (totalScore === randomNumber) {
+			alert("Match!");
+			wins++;
+			$("#wins").html(wins);
+			console.log(wins);
+		}
+
+		else if (totalScore >= randomNumber) {
+			alert("Over!");
+			losses++;
+			$("#losses").html(losses);
+			console.log(losses);
+		}
 	});
 
 	$("#crystal-2").on("click", function() {
 		console.log(randomCrystal);
 
+		totalScore += randomCrystal;
 
+		$("#number").html(totalScore);
+
+		if (totalScore === randomNumber) {
+			alert("Match!");
+			wins++;
+			$("#wins").html(wins);
+			console.log(wins);
+		}
+
+		else if (totalScore >= randomNumber) {
+			alert("Over!");
+			losses++;
+			$("#losses").html(losses);
+			console.log(losses);
+		}
+	});
+
+	$("#crystal-3").on("click", function() {
+		console.log(randomCrystal);
+
+		totalScore += randomCrystal;
+
+		$("#number").html(totalScore);
+
+		if (totalScore === randomNumber) {
+			alert("Match!");
+			wins++;
+			$("#wins").html(wins);
+			console.log(wins);
+		}
+
+		else if (totalScore >= randomNumber) {
+			alert("Over!");
+			losses++;
+			$("#losses").html(losses);
+			console.log(losses);
+		}
+	});
+
+	$("#crystal-4").on("click", function() {
+		console.log(randomCrystal);
+
+		totalScore += randomCrystal;
+
+		$("#number").html(totalScore);
+
+		if (totalScore === randomNumber) {
+			alert("Match!");
+			wins++;
+			$("#wins").html(wins);
+			console.log(wins);
+		}
+
+		else if (totalScore >= randomNumber) {
+			alert("Over!");
+			losses++;
+			$("#losses").html(losses);
+			console.log(losses);
+		}
 	});
 
 
