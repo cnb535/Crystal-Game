09c35425dbbe0bc7a85cb03dcb0a935e7920aecf
Christina Bertram <Jamerican92grl@MacBook-Pro.home>
2018-04-23 23:12:29 -0400

    The game.js file has been updated. The buttons now have click events on them.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,34 @@
-const getRandomIntInclusive = function(min, max) {
-	min = Math.ceil(min);
-	max = Math.floor(max);
-	return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+// $(document).ready(function() {
+
+	let totalScore = 0;
+	let wins = 0;
+	let losses = 0;
+
+	const getRandomIntInclusive = function(min, max) {
+		min = Math.ceil(min);
+		max = Math.floor(max);
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
 
 
-const randomNumber = getRandomIntInclusive(120, 19);
-console.log(randomNumber);
+	const randomNumber = getRandomIntInclusive(19, 120);
+	console.log(randomNumber);
 
-const randomNumberDiv = document.getElementById("random-number");
+	$("#random-number").html(randomNumber);
 
-randomNumberDiv.innerHTML = randomNumber;
+	const randomCrystal = getRandomIntInclusive(1,12);
+
+	$("#crystal-1").on("click", function() {
+		console.log(randomCrystal);
+
+
+	});
+
+	$("#crystal-2").on("click", function() {
+		console.log(randomCrystal);
+
+
+	});
+
+
+// });
